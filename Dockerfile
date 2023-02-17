a8a8f7dffd401787d2c004f8fdae509128c6cd14
badma <badma.chedyrov@yahoo.com>
2023-02-17 12:01:18 +0300

    1st commit

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM adoptopenjdk/openjdk11:alpine-jre
+LABEL "name"="evraz"
+ARG JAR_FILE=target/evraz-0.0.1-SNAPSHOT.jar
+WORKDIR /opt/app
+COPY ${JAR_FILE} app.jar
+ENTRYPOINT ["java","-jar","app.jar"]
--- src/main/java/kafka/kafka_consumer/config/KafkaConfig.java
+++ src/main/java/kafka/kafka_consumer/config/KafkaConfig.java
@@ -1,7 +1,9 @@
 package kafka.kafka_consumer.config;
 
+import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.config.SslConfigs;
 import org.apache.kafka.common.serialization.LongSerializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
@@ -17,10 +19,27 @@ import java.util.Map;
 
 @Configuration
 public class KafkaConfig {
-    @Value("${spring.kafka.consumer.bootstrap-servers}")
+    @Value("${spring.kafka.bootstrap-servers}")
     private String kafkaServer;
+    @Value("${spring.kafka.consumer.auto-offset-reset}")
+    private String consumerAutOffsetReset;
     @Value("${spring.kafka.consumer.group-id}")
     private String kafkaGroupId;
+    @Value("${spring.kafka.properties.sasl.mechanism}")
+    private String propertiesSaslMechanism;
+    @Value("${spring.kafka.properties.security.protocol}")
+    private String propertiesSecurityProtocol;
+    @Value("${spring.kafka.properties.sasl.jaas.config}")
+    private String propertiesJaasConfig;
+    @Value("${spring.kafka.consumer.max-poll-records}")
+    private String consumerMaxPollRecords;
+    @Value("${spring.kafka.consumer.enable-auto-commit}")
+    private String consumerEnableAutoCommit;
+    @Value("${spring.kafka.properties.ssl.truststore-location}")
+    private String propertiesSslKeystoreLocation;
+    @Value("${spring.kafka.properties.ssl.truststore-password}")
+    private String propertiesSslKeystorePassword;
+
 
     @Bean
     public Map<String, Object> consumerConfigs() {
@@ -32,6 +51,19 @@ public class KafkaConfig {
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
                 StringSerializer.class);
         props.put(ConsumerConfig.GROUP_ID_CONFIG, kafkaGroupId);
+        props.put("sasl.mechanism", propertiesSaslMechanism);
+
+        props.put("sasl.jaas.config", propertiesJaasConfig);
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, consumerAutOffsetReset);
+        props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, consumerMaxPollRecords);
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, consumerEnableAutoCommit);
+        props.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, propertiesSecurityProtocol);
+        props.put("ssl.truststore.location", propertiesSslKeystoreLocation);
+        props.put("ssl.truststore.password", propertiesSslKeystorePassword);
+
+
+
+
         return props;
     }
 
